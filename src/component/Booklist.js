e8099b34b9adca70665ce629eef8501f17232a5a
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-10-21 15:52:28 +0300

    component to display books from graphql

--- src/component/Booklist.js
+++ src/component/Booklist.js
@@ -1,11 +1,52 @@
+import { useQuery } from '@apollo/client';
+import { getBooksQuery } from '../queries/queries';
+
+//functional component
 const Booklist = () => {
+    const {loading, error, data } = useQuery(getBooksQuery);
+    // console.log(`loading: ${loading}\ndata: ${data}\nerror: ${error}`)
+    // console.log(data.books)
+
+    const displayBooks = () => {
+        if(loading){
+            return ( <div>Loading books...</div> )
+        }
+        if(error){
+            return ( <div>Error: {error.message}</div> )
+        }
+        else {
+            return data.books.map(book => {
+                return(<li key={book.id} >{book.name}</li>)
+            }) 
+        }
+
+    }
+
     return ( 
         <div>
             <ul id="book-list">
-                <li>Book</li>
+                { displayBooks()}
             </ul>
         </div>
      );
 }
  
 export default Booklist;
+
+
+//class component
+// class BookList extends Component{
+//     //tobedone
+//     render(){
+//         return(
+//             <div>
+//                 <ul id="book-list">
+//                     <li>Book Name</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+// export default graphql(getBooksQuery)(BookList);
+
