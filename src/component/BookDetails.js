cda8757bf0df88646c933de8a5d740d73f2896da
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-10-23 20:26:41 +0300

    successfully displayed individual book details in the DOM when u select any

--- src/component/BookDetails.js
+++ src/component/BookDetails.js
@@ -3,11 +3,13 @@ import { getBookQuery } from '../queries/queries';
 
 //functional component
 const BookDetails = ({ bookId }) => {
-    const {loading, error, data: book } = useQuery(getBookQuery, {
-        variables: { bookId }
+    const {loading, error, data } = useQuery(getBookQuery, {
+        variables: { id: bookId }
     });
     // console.log(`loading: ${loading}\ndata: ${data}\nerror: ${error}`)
-    // console.log(bookId)
+    // if(data){
+    //     console.log(data.book)
+    // }
 
     const displayBookDetails = () => {
         if(loading){
@@ -17,14 +19,15 @@ const BookDetails = ({ bookId }) => {
             return ( <div>Error: {error.message}</div> )
         }
         else {
-            if(bookId){
+            if(bookId !== null){
+                let book = data.book;
                 return(
                     <div>
                         <h2>{book.name}</h2>
                         <p>{book.genre}</p>
                         <p>{book.author.name}</p>
                         <ul className='other-books'>
-                            {book.author.map(item => {
+                            {book.author.books.map(item => {
                                 return <li key={item.id}>{item.name}</li>
                             })}
                         </ul>
